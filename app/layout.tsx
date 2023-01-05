f7e7ea204a4390550af556c143537cc830256d4d
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-01-05 13:13:03 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/globals.css'
+import NavBar from './components/nav-bar'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  )
+}
